c053aa57987ecdfe018ebda2315d5a7c16af858d
Jessy <jessy.lancaster@gmail.com>
2024-05-22 21:00:07 +0000

    Add customers query

---
+++ customers.sql
@@ -0,0 +1,18 @@
+with orders_agg as (
+  select 
+  customer_id
+  ,min(orders.created_at) as first_order_at
+  ,count(orders.id) as total_orders
+from `analytics-engineers-club.coffee_shop.orders` as orders
+group by customer_id
+)
+select 
+  customers.id as customer_id
+  ,customers.name
+  ,customers.email
+  ,orders_agg.*
+from `analytics-engineers-club.coffee_shop.customers` as customers
+left join orders_agg
+on customers.id = orders_agg.customer_id
+order by first_order_at
+limit 5;
